960182e3b32e31781c0d49678cc483d299776b03
SJ coded <who@sjcoded.com>
2021-12-14 12:22:23 +1100

    line-drawing

---
+++ line-drawing/Dollar.js
@@ -0,0 +1,11 @@
+anime({
+    targets: '.dollar .💲',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1500,
+    delay: function(el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+  });
+
+  
